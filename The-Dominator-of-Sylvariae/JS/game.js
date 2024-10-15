49de17404ab4e1a91564db70304e41b7f385ac25
ShaynaMBarclay <shaynambarclay@gmail.com>
2024-10-15 18:50:12 +0200

    updated game base, before extras are added

--- The-Dominator-of-Sylvariae/JS/game.js
+++ The-Dominator-of-Sylvariae/JS/game.js
@@ -3,9 +3,16 @@ class Game {
         this.startScreen = document.getElementById("Sylvariae-intro");
         this.gameScreen = document.getElementById("Sylvariae-container");
         this.endScreen = document.getElementById("Sylvariae-end");
-        this.player = new Player(10, 10, 300, 300, "/images/player.png");
         this.height = 600;
         this.width = 500;
+        // sets the Players starting postion to the Right side
+        const playerWidth = 200;
+        const playerStartX = this.width - playerWidth - 10;
+        const playerStartY = 500;
+
+       // this.player = new Player(10, 10, 300, 300, "/images/player.png");
+       this.player = new Player(playerStartX, playerStartY, playerWidth, 200, "/images/player.png");
+
         this.obstacles = [new Obstacle(0, "/images/obstacle1.png"), new Obstacle(0, "/images/obstacle2.png")];
         this.score = 0;
         this.lives = 3;
@@ -51,25 +58,27 @@ class Game {
             console.log("did it collide", didCollide);
 
         //if the player collides with an obstacle
-       // if (this.player.didCollide(Obstacle)) {
        if (didCollide) {
          Obstacle.element.remove();
          this.obstacles.splice(i, 1);
-         //this.lives--;
+         this.lives--;
+         console.log(`Lives Remaining: ${this.lives}`);
          i--;
-        }  else if (Obstacle.top > this.height) {
-           
-            Obstacle.element.remove();
-            this.obstacles.splice(i, 1);
-            this.lives--;
-            i--;
-            this.score++;
-        } 
-    } 
-        if (this.lives === 0) {
+
+         //check is lives are zero
+         if (this.lives === 0) {
             console.log("You died, no more lives");
             this.endGame();
+            return; // exit the function
         } 
+        }  else if (Obstacle.top > this.height) {
+            Obstacle.element.remove();
+            this.obstacles.splice(i, 1);
+            this.score++;
+            i--;
+        } 
+    } 
+    //spawning new obstacles
         if (Math.random() > 0.98 && this.obstacles.length < 1) {
             this.obstacles = [new Obstacle(0, "/images/obstacle1.png"), new Obstacle(null, "/images/obstacle2.png")];
         }
